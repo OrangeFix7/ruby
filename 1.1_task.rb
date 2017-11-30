4006ddb8002c969def1a756d06783a845934849d
OrangeFix7 <budwayse@yandex.ru>
2017-11-30 12:06:13 +0200

    Добавлены решения

---
+++ 1.1_task.rb
@@ -0,0 +1,6 @@
+def add_numbers(one,two)
+	puts one + two
+end
+
+add_numbers(3,4)
+
---
+++ 1.2_task.rb
@@ -0,0 +1,9 @@
+def compare_numbers (one,two)
+	if one > two 
+		puts one-two
+	else 
+		puts one+two
+	end
+end
+
+compare_numbers(5,4)
